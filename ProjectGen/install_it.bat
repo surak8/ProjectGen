7767f4b9285ac3ecc86a5710aae1420c798c6e2a
Rik Cousens <rcousens@colt.com>
2016-06-08 12:50:04 -0400

    changed output-path and install bat-file.

--- ProjectGen/install_it.bat
+++ ProjectGen/install_it.bat
@@ -1,6 +1,13 @@
 @echo off
 setlocal
+set PATH=%ProgramFiles(x86)%\msbuild\14.0\bin;%PATH%
+set OUTPATH=%~dp0bin\release\
+msbuild -nologo projectgen.sln -t:clean -p:configuration=release -v:m
+msbuild -nologo projectgen.sln -t:clean -p:configuration=debug -v:m
 msbuild -nologo projectgen.sln -t:rebuild -p:configuration=release -v:m
-if exist projectgen.pdb copy /y projectgen.pdb \usr\local\bin 
-if exist projectgen.xml copy /y projectgen.xml \usr\local\bin 
-copy /y projectgen.exe \usr\local\bin 
+if exist %OUTPATH%projectgen.pdb copy /y %OUTPATH%projectgen.pdb \usr\local\bin 
+if exist %OUTPATH%projectgen.xml copy /y %OUTPATH%projectgen.xml \usr\local\bin 
+copy /y %OUTPATH%projectgen.exe \usr\local\bin 
+echo.
+echo hit ENTER
+pause
